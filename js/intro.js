671c2c9ef9654b112cccdae87d3812259ed18a2e
Andreza Cri <andrezadesenvolvedoraweb@gmail.com>
2023-04-24 19:21:11 -0300

    first commit

---
+++ js/intro.js
@@ -0,0 +1,63 @@
+
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+const intro = document.getElementsByClassName('intro')[0];
+const story = document.getElementsByClassName('story')[0];
+const paragraphs = document.getElementsByClassName('paragraphs')[0];
+const sound = document.getElementById('sound');
+
+intro.style.fontSize = width / 30 + 'px';
+story.style.fontSize = width / 20 + 'px';
+paragraphs.style.height = height + 'px';
+
+window.addEventListener('resize', () => {
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+  intro.style.fontSize = width / 30 + 'px';
+  story.style.fontSize = width / 20 + 'px';
+  paragraphs.style.height = height + 'px';
+});
+
+function start() {
+  intro.className = 'intro text_intro animation_intro';
+  story.className = 'story text_story animation_story';
+  sound.play();
+}
+
+/* Background with Stars */
+
+let canvas = document.getElementById('snow');
+const ctx = canvas.getContext('2d');
+
+canvas.width = width;
+canvas.height = height;
+
+const num = 100;
+const size = 2;
+const elements = [];
+
+function starts() {
+  for (let i = 0; i < num; i++) {
+    elements[i] = {
+      x: Math.ceil(Math.random() * width),
+      y: Math.ceil(Math.random() * height),
+      size: Math.random() * size,
+    };
+  }
+}
+
+function snow() {
+  ctx.clearRect(0, 0, width, height);
+  for (let i = 0; i < num; i++) {
+    const e = elements[i];
+    ctx.beginPath();
+    ctx.fillStyle = '#FFFFFF';
+    ctx.arc(e.x, e.y, e.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
+
+starts();
+snow();
